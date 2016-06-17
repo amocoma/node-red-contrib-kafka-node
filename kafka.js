3ad34afb9b72f78132c7af810cee1559c86e5f29
Dirk <dirk.gronert@gmail.com>
2016-06-17 19:17:35 +0200

    typo

--- kafka.js
+++ kafka.js
@@ -8,50 +8,34 @@ module.exports = function(RED) {
      - topics 
      - zkquorum(example: zkquorum = “[host]:2181")
      */
-    function kafkaNode(config) {
-        console.log('here you go');
+    function kafkaProducerNode(config) {
         RED.nodes.createNode(this,config);
-        var topic = config.topic;
-        var clusterZookeeper = config.zkquorum;
         var node = this;
-        var kafka = require('kafka-node');
-        var HighLevelProducer = kafka.HighLevelProducer;
-        var Client = kafka.Client;
-        var topics = config.topics;
-        var client = new Client(clusterZookeeper);
-
-        try {
-            this.on("input", function(msg) {
-                var payloads = [];
-
-                // check if multiple topics
-                if (topics.indexOf(",") > -1){
-                    var topicArry = topics.split(',');
-
-                    for (i = 0; i < topicArry.length; i++) {
-                        payloads.push({topic: topicArry[i], messages: msg.payload});
-                    }
-                }
-                else {
-                    payloads = [{topic: topics, messages: msg.payload}];
-                }
-
-                producer.send(payloads, function(err, data){
-                    if (err){
-                        node.error(err);
-                    }
-                    node.log("Message Sent: " + data);
+        // Retrieve the config node
+        this.server = RED.nodes.getNode(config.server);
+        if (this.server) {
+            var kafka = require('kafka-node'),
+                hlProducer = kafka.HighLevelProducer,
+                clusterZookeeper = this.server.zkquorum,
+                topics = String(config.topics),
+                zkOptions = {key:this.server.key, cert:this.server.cert, ca:this.server.ca},
+                client = new kafka.Client(clusterZookeeper, zkOptions),
+            try {
+                this.on("input", function(msg) {
+                    var payloads = [];
+                    topics.split(',').forEach(function(_topic){payloads.push({topic: _topic.trim(), messages: msg.payload});});
+                    producer.send(payloads, function(err, data){if(err)node.error(err); else node.log("Message Sent: " + data);});
                 });
-            });
+            }catch(e) {
+                node.error(e);
+            }
+            var producer = new HighLevelProducer(client);
+        }else{
+            node.log('No config node configured');
         }
-        catch(e) {
-            node.error(e);
-        }
-        var producer = new HighLevelProducer(client);
     }
 
-    RED.nodes.registerType("kafka",kafkaNode);
-
+    RED.nodes.registerType("kafka-prod",kafkaProducerNode);
 
     function kafkaSubscriberNode(config) {
         RED.nodes.createNode(this,config);
@@ -62,25 +46,20 @@ module.exports = function(RED) {
         if (this.server) {
             var hlConsumer = kafka.HighLevelConsumer,
                 topics = String(config.topics),
-                clusterZookeeper = server.zkquorum,
-                zkOptions = {key:server.key, cert:server.cert, ca:server.ca},
+                clusterZookeeper = this.server.zkquorum,
+                zkOptions = {key:this.server.key, cert:this.server.cert, ca:this.server.ca},
                 client = new (require('kafka-node')).Client(clusterZookeeper, zkOptions),
                 kafkaOptions = {
                     groupId: config.groupId,
                     autoCommit: config.autoCommit,
                     autoCommitMsgCount: 10,
-                };
+                },
                 topicJSONArry = [];
             if(topics!=null && topics.trim().length > 0){
-                if (topics.indexOf(",") != -1){
                     topics.split(',').forEach(function(_topic){topicJSONArry.push({topic: _topic.trim()});});
-                    topics = topicJSONArry;
-                }else{
-                    topics = [{topic:topics.trim()}];
-                }
                 try {
-                    var consumer = new hlConsumer(client, topics, options);
-                    this.log("Consumer created...");
+                    var consumer = new hlConsumer(client, topicJSONArry, kafkaOptions);
+
 
                     consumer.on('message', function (message) {
                         console.log(message);
