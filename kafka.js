4fa9bd323b03ce8a3dea2f810cc5c5ddf0bcc7bf
Dirk <dirk.gronert@gmail.com>
2016-06-26 01:43:04 +0200

    adding ssl/tls

--- kafka.js
+++ kafka.js
@@ -40,18 +40,17 @@ module.exports = function(RED) {
 
     function kafkaSubscriberNode(config) {
         RED.nodes.createNode(this,config);
-        var Promise = require('bluebird');
-        var node = this;
-        var Kafka = require('no-kafka');
-
+        var node = this,
+            Promise = require('bluebird'),
+            Kafka = require('no-kafka'),
+            env = node.context().global.get('process').env;
         // Retrieve the config node
         this.server = RED.nodes.getNode(config.server);
-        var env = node.context().global.get('process').env;
+
         if (this.server) {
-            var clusterZookeeper = this.server.zkquorum,
-                topics = String(config.topics), // not used right now!
+            var clusterZookeeper = this.    server.zkquorum,
                 sslOptions = {key:env.KAFKA_CLIENT_CERT_KEY, cert:env.KAFKA_CLIENT_CERT, ca:env.KAFKA_TRUSTED_CERT, checkServerIdentity: function (host, cert) {return undefined;}},
-                zkOptions = {connectionString:env.KAFKA_URL, ssl: sslOptions},
+                zkOptions = {connectionString: env.KAFKA_URL, ssl: sslOptions},
                 consumer = new Kafka.GroupConsumer(zkOptions);
             var dataHandler = function (messageSet, topic, partition) {
                 return Promise.each(messageSet, function (m){
@@ -71,42 +70,6 @@ module.exports = function(RED) {
 
             consumer.init(strategies); // all done, now wait for messages in dataHandler
 
-
-            /**
-            var hlConsumer = kafka.HighLevelConsumer,
-                topics = String(config.topics),
-                clusterZookeeper = this.server.zkquorum,
-                zkOptions = {key:this.server.key, cert:this.server.cert, ca:this.server.ca},
-                client = new (require('kafka-node')).Client(clusterZookeeper, zkOptions),
-                kafkaOptions = {
-                    groupId: config.groupId,
-                    autoCommit: config.autoCommit,
-                    autoCommitMsgCount: 10,
-                },
-                topicJSONArry = [];
-            if(topics!=null && topics.trim().length > 0){
-                    topics.split(',').forEach(function(_topic){topicJSONArry.push({topic: _topic.trim()});});
-                try {
-                    var consumer = new hlConsumer(client, topicJSONArry, kafkaOptions);
-
-
-                    consumer.on('message', function (message) {
-                        console.log(message);
-                        node.log(message);
-                        var msg = {payload: message};
-                        node.send(msg);
-                    });            
-                    consumer.on('error', function (err) {
-                       console.error(err);
-                    });
-                }catch(e){
-                    node.error(e);
-                    return;
-                } 
-            }else{
-                console.error('No topics configures');
-            }
-            **/
         }else{
             node.log('No config node configured');
             // 
